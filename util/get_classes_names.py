6de1e48ad956d8192633b08dd009f8fa1c5ddb6b
Felipe Seolin <felipe.seolin@hotmail.com>
2021-11-22 02:30:06 -0300

    Util to get all classes name

---
+++ util/get_classes_names.py
@@ -0,0 +1,17 @@
+import os
+
+location = '/Users/seolin/Documents/TCC/image-texture-classification-tests/datasets/brodatz/images'
+images = sorted(os.listdir(location))
+classes = []
+
+for image in images:
+    if os.path.isdir(image):
+        continue
+
+    class_name = image.split('_')[0]
+    if class_name in classes:
+        continue
+
+    classes.append(class_name)
+
+print("', '".join(classes))
