9367597ce529f3e39d0a6cc938b29dab9999e1a7
Felipe Seolin <felipe.seolin@hotmail.com>
2021-11-22 17:41:38 -0300

    Add USPTex dataset support

--- TestBase.py
+++ TestBase.py
@@ -94,6 +94,8 @@ class TestBase:
                 file_path = file_path + 'kylberg-texture-dataset/'
             if self.dataset == dataset_constants.BRODATZ:
                 file_path = file_path + 'brodatz-texture-dataset/'
+            if self.dataset == dataset_constants.USPTEX:
+                file_path = file_path + 'usptex-texture-dataset/'
 
             file_path = file_path + 'data/'
             # Descriptor
@@ -131,6 +133,8 @@ class TestBase:
                 path = path + 'kylberg_texture_dataset/'
             if self.dataset == dataset_constants.BRODATZ:
                 path = path + 'brodatz_texture_dataset/'
+            if self.dataset == dataset_constants.USPTEX:
+                path = path + 'usptex_texture_dataset/'
             # Classifier
             if self.classifier == classifier_constants.DECISION_TREE:
                 path = path + 'decision_tree'
@@ -173,6 +177,8 @@ class TestBase:
                 header = header + 'KYLBERG TEXTURE DATASET'
             if self.dataset == dataset_constants.BRODATZ:
                 header = header + 'BRODATZ TEXTURE DATASET'
+            if self.dataset == dataset_constants.BRODATZ:
+                header = header + 'USPTEX TEXTURE DATASET'
 
             return header + " ======"
 
@@ -180,6 +186,22 @@ class TestBase:
         if self.dataset == dataset_constants.BRODATZ:
             return [
                 'BAR', 'BRI', 'BUB', 'GRA', 'LEA', 'PIG', 'RAF', 'SAN', 'STR', 'WAT', 'WEA', 'WOL', 'WOO'
+            ]
+        if self.dataset == dataset_constants.USPTEX:
+            return [
+                '001', '002', '003', '004', '005', '006', '007', '008', '009', '010', '011', '012', '013', '014', '015',
+                '016', '017', '018', '019', '020', '021', '022', '023', '024', '025', '026', '027', '028', '029', '030',
+                '031', '032', '033', '034', '035', '036', '037', '038', '039', '040', '041', '042', '043', '044', '045',
+                '046', '047', '048', '049', '050', '051', '052', '053', '054', '055', '056', '057', '058', '059', '060',
+                '061', '062', '063', '064', '065', '066', '067', '068', '069', '070', '071', '072', '073', '074', '075',
+                '076', '077', '078', '079', '080', '081', '082', '083', '084', '085', '086', '087', '088', '089', '090',
+                '091', '092', '093', '094', '095', '096', '097', '098', '099', '100', '101', '102', '103', '104', '105',
+                '106', '107', '108', '109', '110', '111', '112', '113', '114', '115', '116', '117', '118', '119', '120',
+                '121', '122', '123', '124', '125', '126', '127', '128', '129', '130', '131', '132', '133', '134', '135',
+                '136', '137', '138', '139', '140', '141', '142', '143', '144', '145', '146', '147', '148', '149', '150',
+                '151', '152', '153', '154', '155', '156', '157', '158', '159', '160', '161', '162', '163', '164', '165',
+                '166', '167', '168', '169', '170', '171', '172', '173', '174', '175', '176', '177', '178', '179', '180',
+                '181', '182', '183', '184', '185', '186', '187', '188', '189', '190', '191'
             ]
         if self.dataset == dataset_constants.KYLBERG:
             return [
@@ -247,6 +269,9 @@ class TestBase:
 
     def set_kylberg_dataset(self):
         self.dataset = dataset_constants.KYLBERG
+
+    def set_usptex_dataset(self):
+        self.dataset = dataset_constants.USPTEX
 
     def set_brodatz_dataset(self):
         self.dataset = dataset_constants.BRODATZ
---
+++ lbp/usptex_texture_dataset/SVC/main.py
@@ -0,0 +1,13 @@
+from TestBase import TestBase
+
+
+def main():
+    test_base = TestBase()
+    test_base.set_svc_classifier()
+    test_base.set_lbp_descriptor()
+    test_base.set_usptex_dataset()
+    test_base.test_model()
+
+
+if __name__:
+    main()
